aa92de9ceb8891e11de628f0d8f81c0b72c146d3
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2024-06-18 20:10:27 +0900

    fix: Folder Modal 안내 메시지 변경

--- src/domain/folder/component/setting-folder/FolderList.tsx
+++ src/domain/folder/component/setting-folder/FolderList.tsx
@@ -35,8 +35,7 @@ export default function FolderList({
     <>
       <h1 className="text-left text-lg font-bold mt-4 px-2">{title}</h1>
       <div className="border-2 border-success bg-green-50 rounded-box gap-2">
-        {folders &&
-          folders.map((folder: Folder, index: number) => (
+        {folders && folders.map((folder: Folder, index: number) => (
             <FolderBox
               key={index}
               folder={folder}
